bc2175a216fea7198e9c1c042e4712ec580954fd
Talison Henke <talison.henke@gmail.com>
2025-11-05 21:34:28 -0300

    base domain qr code manual

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -115,7 +115,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const qrModal = document.getElementById("qrModal");
     const closeModal = document.getElementById("closeQrModal");
     const scanResult = document.getElementById("scanResult");
-    const baseDomain = "{{ url('/') }}";
+    // const baseDomain = "{{ url('/') }}";
+    
+    const baseDomain = "https://sicaonline.com.br";
     let html5QrCode;
 
     qrBtn.addEventListener("click", () => {
